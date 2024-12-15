35c44a00f23f16a806156ee13f57b782974b1a7e
dyddlfsla <7942dyddlfsla@gmail.com>
2024-12-15 23:35:56 +0900

    fix: gradle run error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,14 @@
 module org.example.baramdemo {
-  requires javafx.fxml;
 
+  requires javafx.fxml;
   requires org.controlsfx.controls;
   requires org.kordamp.bootstrapfx.core;
   requires com.github.kwhat.jnativehook;
+  requires opencv;
 
   opens org.example.baramdemo to javafx.fxml;
-  exports org.example.baramdemo;
-  exports org.example.baramdemo.controller;
   opens org.example.baramdemo.controller to javafx.fxml;
+
+  exports org.example.baramdemo.controller;
+  exports org.example.baramdemo;
 }
